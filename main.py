fc046083475671ec61047b1775c258f9e46d99db
Tuman8 <i-a-l-e-x@yandex.ru>
2022-03-11 10:51:24 +0700

    реализов вывод главной странички

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
+from utils import get_posts_all
 
 app = Flask(__name__)
 
 @app.route('/')
 def page_main():
-    return render_template('index.html')
+    posts = get_posts_all()
+    count_posts = len(posts)
+    return render_template('index.html', posts=posts, count_posts=count_posts)
 
 
 if __name__ == '__main__':
